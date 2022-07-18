a96e97db47a92093a38bc24355d616e79c487fb7
jaewoong2 <jwisgenius@naver.com>
2022-07-18 23:59:10 +0900

    :tada: 개발 환경 세팅
    Co-authored-by: intersoom <sallybig@naver.com>
    Co-authored-by: Twince <tjsdn811@gmail.com>

---
+++ realworld/.eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx', '.tsx'] }],
+    semi: 'off',
+    '@typescript-eslint/semi': ['error'],
+    'no-shadow': ['off', {}],
+    'import/prefer-default-export': 'off',
+    indent: ['error', 2],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
