9257f46c6babf683edbbae2084a2002b86468d77
jaewoong2 <jwisgenius@naver.com>
2022-07-19 19:53:17 +0900

    :lipstick: Implement Header UI
    Co-authored-by: intersoom <sallybig@naver.com>
    Co-authored-by: Twince <tjsdn811@gmail.com>

--- realworld/src/features/auth/hooks/useFetch.ts
+++ realworld/src/features/auth/hooks/useFetch.ts
@@ -5,27 +5,49 @@ import { useEffect, useState } from 'react';
 type FetchType = {
     url: string
     method: 'POST' | 'GET'
-    body: string
+    body?: string
 };
-// // useFetch('',asd)
-
-// // unknown === any
 
 export const useFetch = <T = unknown>({ url, method, body }: FetchType) => {
   const [value, setValue] = useState<T>();
 
-  const fetchData = async () => {
-    try {
-      const data = await fetch(url, { method, body });
-      setValue(data as T);
-    } catch (err) {
-      setValue(err as T);
-    }
-  };
+  if (method === 'GET') {
+    const fetchData = async () => {
+      try {
+        const data = await fetch(url, {
+          method,
+          headers: {
+            'Content-type': 'application/json',
+          },
+        });
+        setValue(data as T);
+      } catch (err) {
+        setValue(err as T);
+      }
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+      fetchData();
+    }, []);
+  } else {
+    const fetchData = async () => {
+      try {
+        const data = await fetch(url, {
+          method,
+          body,
+          headers: {
+            'Content-type': 'application/json',
+          },
+        });
+        setValue(data as T);
+      } catch (err) {
+        setValue(err as T);
+      }
+    };
 
+    useEffect(() => {
+      fetchData();
+    }, []);
+  }
   return value;
 };
