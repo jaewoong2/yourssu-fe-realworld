a236fff0c5e0dbb3e5468a35aca8c99098644d81
jaewoong2 <jwisgenius@naver.com>
2022-07-20 09:45:14 +0900

    :bug: fixing eslint
    Co-authored-by: intersoom <sallybig@naver.com>
    Co-authored-by: Twince <tjsdn811@gmail.com>

--- realworld/.eslintrc.js
+++ realworld/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
