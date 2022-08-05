00b8f5c12906b3cf6c33936e7eae562338f0aeba
jaewoong2 <jwisgenius@naver.com>
2022-08-05 12:42:14 +0900

    :zap: ref 사용을 위한 forwardRef 사용
    Co-authored-by: intersoom <sallybig@naver.com>
    Co-authored-by: Twince <tjsdn811@gmail.com>

--- realworld/src/features/auth/components/Input/index.tsx
+++ realworld/src/features/auth/components/Input/index.tsx
@@ -1,15 +1,13 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { forwardRef, InputHTMLAttributes } from 'react';
 
-type InputProps = {
-  ref?: React.RefObject<HTMLInputElement>;
-} & InputHTMLAttributes<HTMLInputElement>;
+type InputProps = {} & InputHTMLAttributes<HTMLInputElement>;
 
-function Input({ className, ref, ...props }: InputProps) {
+const Input = forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
   return (
     <fieldset className="form-group">
       <input ref={ref} className={`form-control form-control-lg ${className}`} {...props} />
     </fieldset>
   );
-}
+});
 
 export default Input;
