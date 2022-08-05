a15ec5a6a143c0bdb9d0950f44c88a2b4c17ed1f
jaewoong2 <jwisgenius@naver.com>
2022-08-05 12:41:58 +0900

    :recylce: SignIn, SignUp 리팩토링
    Co-authored-by: intersoom <sallybig@naver.com>
    Co-authored-by: Twince <tjsdn811@gmail.com>

--- realworld/src/features/auth/components/SigninForm.tsx
+++ realworld/src/features/auth/components/SigninForm.tsx
@@ -1,93 +1,26 @@
-import React, { useCallback } from 'react';
-import { useMutation } from 'react-query';
+import React from 'react';
 import { useForm } from '../hooks/useForm';
-
-import { UserType } from '../recoil/atoms/userState';
-import { z } from 'zod';
 import Input from './Input';
-
-type userResult = z.infer<typeof userResult>;
-const userResult = z.object({
-  user: z.object({
-    email: z.string(),
-    token: z.string(),
-    username: z.string(),
-    bio: z.string(),
-    image: z.string(),
-  }),
-  errors: z.object({
-    email: z.array(z.string()).nullable(),
-    password: z.array(z.string()).nullable(),
-    username: z.array(z.string()).nullable(),
-  }).nullable(),
-})
-
-const signInFetch = async ({
-  email,
-  password,
-} : {
-  email: string;
-  password: string;
-}): Promise<userResult | string> => {
-  try {
-    const data = await fetch('https://api.realworld.io/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ user: { email, password } }),
-      headers: {
-        'Content-type': 'application/json',
-      },
-    });
-    if (data.ok) {
-      return await userResult.parse(data);
-    }
-
-  if (data.ok) {
-    return data.json();
-  }
-
-  throw new Error(await data.json());
-};
+import { useSignin } from '../hooks/useSignin';
+import FormLayout from './FormLayout';
+import { useAuthNavigate } from '../hooks/useAuthNavigate';
 
 function SigninForm() {
-  const [email, , handleEmail] = useForm('');
-  const [password, , handlePassword] = useForm('');
-
-  // CRUD {C, U, D} == { POST, PUT, DELETE }
-  const { mutate } = useMutation(signInFetch);
-
-  const handleLogin = useCallback(
-    (e: React.FormEvent) => {
-      e.preventDefault();
-      mutate({ email, password });
-    },
-    [email, password]
-  );
+  const [email, handleEmail] = useForm();
+  const [password, handlePassword] = useForm();
+  const { handleSignin, data } = useSignin({ email, password });
+  useAuthNavigate({ key: 'user', to: '/' }, [data]);
 
   return (
-    <div className="auth-page">
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Sign in</h1>
-            <p className="text-xs-center">
-              <a href="/signup">Need an account?</a>
-            </p>
-            <form onSubmit={handleLogin}>
-              <Input onChange={handleEmail} value={email} type="text" placeholder="email" />
-              <Input
-                onChange={handlePassword}
-                value={password}
-                type="password"
-                placeholder="password"
-              />
-              <button type="submit" className="btn btn-lg btn-primary pull-xs-right">
-                Sign in
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+    <FormLayout aHref="/signup" aMessage="Need an account?" authType="Sign in">
+      <form onSubmit={handleSignin}>
+        <Input onChange={handleEmail} value={email} type="text" placeholder="email" />
+        <Input onChange={handlePassword} value={password} type="password" placeholder="password" />
+        <button type="submit" className="btn btn-lg btn-primary pull-xs-right">
+          Sign in
+        </button>
+      </form>
+    </FormLayout>
   );
 }
 
--- realworld/src/features/auth/components/SignupForm.tsx
+++ realworld/src/features/auth/components/SignupForm.tsx
@@ -1,106 +1,38 @@
-import React, { useCallback } from 'react';
-import { useMutation, useQueryClient } from 'react-query';
+import React from 'react';
+import { validateEmail } from '../../utils';
 import { useForm } from '../hooks/useForm';
-import { UserType } from '../recoil/atoms/userState';
-import { z } from 'zod';
+import { useSignup } from '../hooks/useSignup';
+import FormLayout from './FormLayout';
 import Input from './Input';
 
-const signUpFetch = async ({
-  username,
-  email,
-  password,
-} : {
-  username: string;
-  email: string;
-  password: string;
-}): Promise<UserType | Error> => {
-  try {
-    const data: Response = await fetch('https://api.realworld.io/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ user: { username, email, password } }),
-      headers: {
-        'Content-type': 'application/json',
-      },
-    });
-    if (data.status === 200) {
-      return await data.json();
-    }
-
-    throw new Error();
-  } catch (err) {
-    return err as Error;
-  }
-};
-
-// redux
-// recoil + usequery
-
 function SignupForm() {
-  const [username, , handleUsername] = useForm('');
-  const [email, , handleEmail] = useForm('');
-  const [password, , handlePassword] = useForm('');
-
-  const queryClient = useQueryClient();
-
+  const [username, handleUsername] = useForm();
+  const [email, handleEmail] = useForm();
+  const [password, handlePassword] = useForm();
   // POST PUT DELETE
-  const { mutate, isError } = useMutation(signUpFetch);
-
-  const handleSignup = useCallback(
-    (e: React.FormEvent) => {
-      e.preventDefault();
-      mutate({ username, email, password });
-    },
-    [queryClient, username, email, password]
-  );
-
-  const validateEmail = (userEmail: string) => {
-    const emailRegex =
-      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-    return emailRegex.test(userEmail);
-  };
+  const { isError, handleSignup, error } = useSignup({ email, password, username });
 
   return (
-    <div className="auth-page">
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Sign up</h1>
-            <p className="text-xs-center">
-              <a href="/signin">Have an account?</a>
-            </p>
-            {isError && (
-              <ul className="error-messages">
-                <li>오류발생</li>
-              </ul>
-            )}
-
-            <form onSubmit={handleSignup}>
-              <Input
-                onChange={handleUsername}
-                value={username}
-                type="text"
-                placeholder="Your Name"
-              />
-              <Input onChange={handleEmail} value={email} type="text" placeholder="Email" />
-              {!validateEmail(email) && (
-                <ul className="error-messages">
-                  <li>Email Error</li>
-                </ul>
-              )}
-              <Input
-                onChange={handlePassword}
-                value={password}
-                type="password"
-                placeholder="Password"
-              />
-              <button type="submit" className="btn btn-lg btn-primary pull-xs-right">
-                Sign up
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+    <FormLayout aHref="/signin" aMessage="Have an account?" authType="Sign up">
+      {isError && (
+        <ul className="error-messages">
+          <li>{JSON.stringify(error?.response?.data.errors)}</li>
+        </ul>
+      )}
+      <form onSubmit={handleSignup}>
+        <Input onChange={handleUsername} value={username} type="text" placeholder="Your Name" />
+        <Input onChange={handleEmail} value={email} type="text" placeholder="Email" />
+        {!validateEmail(email) && (
+          <ul className="error-messages">
+            <li>Email Error</li>
+          </ul>
+        )}
+        <Input onChange={handlePassword} value={password} type="password" placeholder="Password" />
+        <button type="submit" className="btn btn-lg btn-primary pull-xs-right">
+          Sign up
+        </button>
+      </form>
+    </FormLayout>
   );
 }
 
